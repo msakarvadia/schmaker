2002cd3a67b849be064e4788bcaad5e96fb27a96
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2018-07-23 17:58:14 -0400

    update client id

--- gcal.js
+++ gcal.js
@@ -1,9 +1,8 @@
       // Your Client ID can be retrieved from your project in the Google
       // Developer Console, https://console.developers.google.com
 //      var CLIENT_ID = '607218385891-i3amg4qiptpgfmtenuqannj1jeskh5lv.apps.googleusercontent.com'; //sayans old key
-      var CLIENT_ID = '1057999827636-f8811je8ccetc6hs15h82ca41gdp59c2.apps.googleusercontent.com';
 	var CLIENT_ID = '1057999827636-afj75l3ej8nb3k7rltscclq4g5t602hc.apps.googleusercontent.com';
-//      var SCOPES = ["https://www.googleapis.com/auth/calendar"]; 
+      var SCOPES = ["https://www.googleapis.com/auth/calendar"]; 
 
       var CAL_ID;
       var EVENT_IDS = [];
